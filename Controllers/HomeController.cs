ff6f1e199cf841c88fa1f7f184e6b964aa5489c9
Michael Johnston <michael.johnston6042@gmail.com>
2023-06-26 15:50:50 -0400

    Added full (temporary) prompt for chat. Chat completion result success

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -33,9 +33,9 @@ namespace ChatBotCoachWebsite.Controllers
             //await _buildKnowledgeBase.UpsertPineconeIndexAsync();
             //TODO: omit this when providing the user question as input somewhere else (probably in the controller)
             //for now, user question is hardcoded to be used as an example question
-            string userQuestion = "What can I learn about target priority?";
-            uint topIndexResults = 5;
-            await _queryPineconeIndex.GetRelevantContextAsync(userQuestion, topIndexResults);
+            string userQuestion = "\nQuestion: What can I learn about target priority?";
+            uint topIndexResults = 2;
+            await _queryPineconeIndex.CreateFullAiPromptAsync(userQuestion, topIndexResults);
             return View();
         }
 
--- Helpers/QueryPineconeIndex.cs
+++ Helpers/QueryPineconeIndex.cs
@@ -17,23 +17,70 @@ namespace ChatBotCoachWebsite.Helpers
             _buildPineconeIndex = buildPineconeIndex;
         }
 
-        private string AiCustomPrompt() => "Answer the question based on the context below";
-
-        private void CreateFullAiPrompt()
+        public async Task CreateFullAiPromptAsync(string userQuestion, uint topIndexResults)
         {
+            List<string> contexts = await GetRelevantContextAsync(userQuestion, topIndexResults);
+            string context = contexts.First();
+
+            string prompt = AiCustomPrompt();
+            string fullCompletionPrompt = prompt + context + userQuestion;
+
+            //get openAI api key
+            var openAiApiKey = _keyProvider.GetKey("openaikey");
+            //initialize openAi
+            OpenAIAPI openAi = new OpenAIAPI(openAiApiKey);
+
+
+            //TODO: Doing chat completion requires a list of chat history. 
+            //      Add method for creating new list of chat messages
+            //      Add method for adding to list of chat messages
+            List<OpenAI_API.Chat.ChatMessage> tempChatList = new();
+
+            OpenAI_API.Chat.ChatMessage chat = new()
+            {
+                Name = "student",
+                Role = OpenAI_API.Chat.ChatMessageRole.User,
+                Content = fullCompletionPrompt
+            };
+
+            tempChatList.Add(chat);
+
+            var result = await openAi.Chat.CreateChatCompletionAsync(tempChatList, model: Model.ChatGPTTurbo);
 
         }
 
-        public async Task GetRelevantContextAsync(string userQuestion, uint topIndexResults)
+        private string AiCustomPrompt() => "Pretend that you are a coach helping teach people about competitive video games, specifically first person shooters. " +
+            "The specific game you will be asked about is Overwatch. " +
+            "Answer the question based on the context below: \n";
+
+        private async Task<List<string>> GetRelevantContextAsync(string userQuestion, uint topIndexResults)
         {
             ScoredVector[]? scoredVectors = await QueryPineconeIndexAsync(userQuestion, topIndexResults);
 
+            List<string> contexts = new();
             foreach(var vector in scoredVectors)
             {
                 MetadataMap metaData = vector.Metadata;
-                
+                foreach(var data in metaData)
+                {
+                    var key = data.Key;
+                    if(key == "name")
+                    {
+                        //do something with name
+                    }
+
+                    if(key == "text")
+                    {
+                        var value = data.Value;
+                        object? text = value.Inner;
+                        contexts.Add(text.ToString());
+                    }
+                }
                 //TODO: Get the relevant context from metaData to be used to create the full ai prompt
+
             }
+
+            return contexts;
         }
 
         private async Task<ScoredVector[]?> QueryPineconeIndexAsync(string userQuestion, uint topIndexResults)
