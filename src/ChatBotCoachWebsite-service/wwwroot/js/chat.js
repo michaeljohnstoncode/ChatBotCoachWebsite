a1bbb38d2dec045863eab5cc236ef51cbb6d6ee6
Michael Johnston <michael.johnston6042@gmail.com>
2023-07-19 16:22:26 -0400

    Added sendMessage js function. And added if enter key pressed, send msg

--- src/ChatBotCoachWebsite-service/wwwroot/js/chat.js
+++ src/ChatBotCoachWebsite-service/wwwroot/js/chat.js
@@ -17,13 +17,34 @@ connection.on("ReceiveMessage", function (user, message) {
 
     // Append the message to the messages list
     document.getElementById("messagesList").appendChild(messageListItem);
+    var messageList = document.getElementById("messagesList");
+    messageList.scrollTop = messageList.scrollHeight;
 });
 
+// Attach click event to the Send button
 document.getElementById("sendButton").addEventListener("click", function (event) {
     event.preventDefault();
+    sendMessage(); // Call the function to send the message
+});
+
+// When enter key is pressed, send message
+document.getElementById("messageInput").addEventListener("keydown", function (event) {
+    // Check if the Enter key was pressed (keyCode 13) and Shift key was not pressed
+    if (event.keyCode === 13 && !event.shiftKey) {
+        event.preventDefault();
+        sendMessage(); // Call the function to send the message
+    }
+});
+
+// Function to send the message
+function sendMessage() {
     var user = document.getElementById("userInput").value;
     var message = document.getElementById("messageInput").value;
 
+    // Check if the message is not empty
+    if (message.trim() === "") {
+        return; // If empty, do not send the message
+    }
 
     // Call the hub method to send the message
     connection.invoke("SendMessage", user, message).catch(function (err) {
@@ -32,7 +53,7 @@ document.getElementById("sendButton").addEventListener("click", function (event)
 
     // Clear the chat input text box
     document.getElementById("messageInput").value = "";
-});
+}
 
 connection.start().then(function () {
     console.log("SignalR connected.");
